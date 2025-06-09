242cb057386a8ec2611cb9bfdde278cd41f6f829
“Quaya <dbrown1@svsu.edu>
2025-06-09 19:34:58 -0400

    corrected background

--- src/ProductTop/ProductTop.js
+++ src/ProductTop/ProductTop.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import './ProductTop.css';
+import mobileVideo from './mobile.mov';
+import desktopVideo from './desktop.mov';
 
 function ProductTop() {
-  const isMobile = window.innerWidth <= 768;  
-  const bgMobile = 'https://i.imghippo.com/files/ni1282ms.gif'; 
-  const bgDesktop = 'https://i.imghippo.com/files/Tj8637oE.gif'; 
+  const isMobile = window.innerWidth <= 768;
+  const selectedVideo = isMobile ? mobileVideo : desktopVideo;
 
   return (
     <section
-      className="Top hero"
-      style={{
-        backgroundImage: `url(${isMobile ? bgMobile : bgDesktop})`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-      }}
-    >
+      className="Top hero">
+
+        <video
+        className="background-video"
+        autoPlay
+        muted
+        loop
+        playsInline
+      >
+        <source src={selectedVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+  
+      </video>
     </section>
   );
 }
